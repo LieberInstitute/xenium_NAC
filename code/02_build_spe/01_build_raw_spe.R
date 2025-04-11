cea636fd6891d8b683c9f8ba84856c4447c879f6
Rphillips1995 <Rphillips11995@gmail.com>
2025-04-11 12:28:25 -0400

    build raw SPE based on code in spatialDLPFC_SCZ_XENIUM

---
+++ code/02_build_spe/01_build_raw_spe.R
@@ -0,0 +1,93 @@
+#Goal: Build raw SPE from first 5 runs of NAc Xenium data
+#cd /dcs05/lieber/marmaypag/xenium_NAC_LIBD4125/xenium_NAC/
+#module load conda_R/4.4.x
+#code modified from https://github.com/LieberInstitute/spatialDLPFC_SCZ_XENIUM/blob/devel/code/analysis/01_build_spe/01_build_spe.R
+
+library(SingleCellExperiment)
+library(SpatialExperiment)
+library(sessioninfo)
+library(tidyverse)
+library(here)
+
+#Load sample_info dataframe that contains information about each sample including depth and directories. 
+sample_info <- read.csv(here("processed-data","Sample_Info_NAcXenium.csv"))
+
+#Add the full sample path to the sample_info dataframe
+sample_info$full_data_path <- file.path(paste(here("raw-data","xenium"),sample_info$Output_Directory,sep = "/"))
+
+#Add  a column of the depth, which is the last aspect of the Sample_ID
+sample_info$Depth <- as.numeric(lapply(strsplit(sample_info$Sample_ID,"_"),"[",3))
+
+#Only going to read first sample in as all of the runs for that one sample are complete. 
+sample_info <- subset(sample_info,subset=(Donor == "Br6660"))
+
+all_spes <- vector(mode = "list",length = nrow(sample_info))
+names(all_spes) <- sample_info$Sample_ID
+for(i in 1:nrow(sample_info)){
+  #Pull path to data and sample_ID
+  sample_path <- sample_info[i,"full_data_path"]
+  Sample <- sample_info[i,"Sample_ID"]
+  
+  print(sprintf("----------%s----------", Sample))
+  
+  sample_info_use <- sample_info[i,]
+  
+  #Paths to counts matrix and cell csv file
+  counts_path <- here(sample_path,"cell_feature_matrix.h5")
+  cell_info_path <- here(sample_path,"cells.csv.gz")
+  
+  #Creae sce object
+  sce <- DropletUtils::read10xCounts(samples = counts_path, sample.names = Sample)
+  counts(sce) <- methods::as(DelayedArray::realize(counts(sce)), "dgCMatrix") # Convert to delayed array
+  
+  cell_info <- vroom::vroom(cell_info_path) #reads in cell csv file much faster than standard functions
+
+  #Add cell info to sce
+  colData(sce) <- cbind(colData(sce),cell_info)
+  spe <- toSpatialExperiment(sce,spatialCoordsNames = c("x_centroid","y_centroid"))
+  rownames(spe) <- rowData(spe)$Symbol #No need to uniquify because Ensembl speicifed during panel generation
+
+  #Need to assign donor-specific column names because we will merge everything after loop is finished. 
+   colnames(spe) <- paste(Sample,rownames(cell_info),sep = "_")
+
+  #Add in relevant metadata. 
+  #First technical variables. 
+  spe$Xenium_Run_ID <- sample_info_use$Xenium_Run_ID
+  spe$Slide_ID <- sample_info_use$Slide_ID
+  spe$Donor <- sample_info_use$Donor
+  spe$CryoSection_Date <- sample_info_use$Date_of_Cryosection
+  spe$Slide_Sample_Reagent_Lot <- sample_info_use$PN.1000460_Lot_Slides_Sample_Reagents
+  spe$Decoding_Reagent_B_Lot <- sample_info_use$PN.1000625_Lot_Decoding_Reagent_Module_B
+  spe$Decoding_Reagent_A_Lot <- sample_info_use$PN.1000624_Lot_Decoding_Reagent_Module_A
+  spe$Human_Brain_Add_On_Lot <- sample_info_use$PN.1000599_Lot_Human_Brain_Add.On
+  spe$Custom_Panel_Lot <- sample_info_use$PN.1000561_Lot_Add.On_Custom_Panel
+  spe$Day_1_Start <- sample_info_use$Day_1_Start
+  spe$Date_Bench_Assay_Completed <- sample_info_use$Date_Bench_Assay_Completed
+  spe$Xenium_Instrument <- sample_info_use$Xenium_Instrument_Used
+  spe$Xenium_Start_Date <- sample_info_use$Start_Date_Xenium_Run
+  
+  #Now biological variables
+  spe$Donor <- sample_info_use$Donor
+  spe$Age <- sample_info_use$Age
+  spe$Sex <- sample_info_use$Sex
+  spe$Race <- sample_info_use$Race
+  spe$PrimaryDx <- sample_info_use$PrimaryDx
+  
+  all_spes[[Sample]] <- spe
+  
+  #clear memory
+  rm(sce)
+  rm(cell_info)
+  gc() #garbage collection
+}
+
+#Combine all of the spes 
+all_spes <- do.call(cbind,all_spes)
+saveRDS(all_spes,here("processed-data","02_build_spe","SPEs","spe_raw.Rds"))
+
+###Reproduciblity
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
---
+++ code/02_build_spe/01_build_raw_spe.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --job-name=01_build
+#SBATCH --output=logs/01_build_raw_spe.log
+#SBATCH --error=logs/01_build_raw_spe.log 
+#SBATCH --mem=20G
+#SBATCH --mail-type=END
+#SBATCH --mail-user=Robert.Phillips@libd.org
+
+echo "********* Job Starts *********"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.4.x
+Rscript 01_build_raw_spe.R
+
+echo "********* Job Ends *********"
+date
