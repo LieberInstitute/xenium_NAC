2c5c66561f24a6b123ea7ae303950c3d6a364088
Rphillips1995 <Rphillips11995@gmail.com>
2025-05-07 21:14:50 -0400

    First pass at Banksy clusters + fix csv file path

---
+++ code/05_clustering/Banksy_clustering.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --job-name=Banksy
+#SBATCH --output=logs/Banksy.log
+#SBATCH --error=logs/Banksy.log 
+#SBATCH --mem=150G
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=Robert.Phillips@libd.org
+
+echo "********* Job Starts *********"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.4.x
+Rscript Banksy_clustering.R
+
+echo "********* Job Ends *********"
+date
