31d771cd61b402ead627ec4650412fbb3e2b00ae
Rphillips1995 <Rphillips11995@gmail.com>
2025-09-24 15:08:37 -0400

    Nonspatial clustering with AGF + louvain at 6 different res values

---
+++ code/05_clustering/Banksy/02_spatial_cluster.R
@@ -0,0 +1,54 @@
+#Run louvain clustering with a variety of resolution values. 
+#cd /dcs05/lieber/marmaypag/xenium_NAC_LIBD4125/xenium_NAC/
+# module load conda_R/4.5
+
+library(SpatialExperiment)
+library(sessioninfo)
+library(optparse)
+library(Banksy)
+library(here)
+
+
+#Pull resolution value from job script. 
+# Define command line options
+option_list <- list(
+  make_option("--res", type = "double", help = "Resolution for clusterBanksy")
+)
+
+# Parse options
+opt <- parse_args(OptionParser(option_list = option_list))
+
+if (is.null(opt$res)) {
+  stop("Error: --res must be provided")
+}
+
+res <- opt$res
+message(sprintf("Running clusterBanksy with resolution=%.2f", res))
+
+#Read in the object
+spe_joint <- readRDS(here("processed-data", "05_Clustering", "SPEs", "spe_joint_banksy_lambda_pt8.RDS"))
+
+spe_joint
+
+
+#Perform banksy clustering. 
+#Run Banksy louvain clustering
+message(paste0("Running Banksy clustering - ", Sys.time()))
+spe_joint <- clusterBanksy(spe_joint, use_agf = TRUE, lambda = 0.8, resolution = res, algo = "louvain",seed = 1000)
+message(paste0("Finished Banksy clustering - ", Sys.time()))
+
+#Save CSV of cluster assigments
+#Pull cluster name
+cluster_name <- colnames(colData(spe_joint))[grep('^clust_', colnames(colData(spe_joint)))]
+cluster_assign <- cbind(colData(spe_joint)[,cluster_name],rownames(colData(spe_joint)))
+head(cluster_assign)
+write.csv(cluster_assign,here("processed-data","05_Clustering","Banksy_Results","Spatial",paste0("Lambdapt8_res",res,"_louvain.csv")))
+
+###Reproduciblity
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
+
+
---
+++ code/05_clustering/Banksy/02_spatial_cluster.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=spatial_banksy_resgrid
+#SBATCH --array=0-5
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=150G
+#SBATCH --time=48:00:00
+#SBATCH --output=logs/%x_%A_%a_spatial.log
+
+echo "********* Job Starts *********"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.5
+
+# Define exactly six resolutions (indices 0..5)
+RES_LIST=(0.25 0.5 0.75 1.0 1.25 1.5)
+
+# Select the resolution corresponding to this array task
+RES="${RES_LIST[$SLURM_ARRAY_TASK_ID]}"
+
+echo ">>> $(date) : Running clusterBanksy with resolution=${RES}"
+Rscript 02_spatial_cluster.R --res "${RES}"
