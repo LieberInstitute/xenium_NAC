1619c865d03d03240953f40ac353def48107d81d
Rphillips1995 <Rphillips11995@gmail.com>
2025-09-28 18:50:56 -0400

    Plot clusters using escheR

---
+++ code/05_clustering/Banksy/03_spatial_plotting_noAGF.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --job-name=03_spatial_noAGF
+#SBATCH --output=logs/03_spatial_noAGF.log
+#SBATCH --error=logs/03_spatial_noAGF.log 
+#SBATCH --mem=250G
+#SBATCH --cpus-per-task=1
+#SBATCH --time=7-00:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=robert.phillips@libd.org
+
+echo "********* Job Starts *********"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.5
+Rscript 03_spatial_plotting_noAGF.R
+
+echo "********* Job Ends *********"
+date
