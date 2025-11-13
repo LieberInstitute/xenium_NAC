1dba26cf7f2ff4151bb53d6c3b6e1a81c6267b11
Rphillips1995 <Rphillips11995@gmail.com>
2025-11-13 15:27:21 -0500

    Max score probability plot

--- code/06_labeltransfer/07_snRNA_label_transfer_exploration.R
+++ code/06_labeltransfer/07_snRNA_label_transfer_exploration.R
@@ -54,3 +54,48 @@ table(colData(spe)$snRNA_predicted_CellType)
 # 13465       27044       21104       26348      146711      297154 
 # Oligo         OPC 
 # 1198472      193499 
+
+#load cluster colors from snRNA-seq paper. 
+load("/dcs04/lieber/marmaypag/spatialNac_LIBD4125/spatial_NAc/processed-data/12_snRNA/070924_21colors_celltypeFinal.rda",verbose = TRUE)
+# Loading objects:
+#   cluster_cols
+
+#Remove neuron ambig
+cluster_cols <- cluster_cols[-14]
+cluster_cols
+# Oligo  DRD1_MSN_A  DRD2_MSN_A         OPC   Microglia   Ependymal 
+# "#4F4753"   "#ECA31C"   "#58B6ED"   "#0D9F72"   "#F2E642"   "#0077B9" 
+# Astrocyte_A  DRD1_MSN_B Endothelial       Inh_A  DRD2_MSN_B Astrocyte_B 
+# "#D95F00"   "#D079AA"   "#D00DFF"   "#35FB00"   "#F80091"   "#FF0016" 
+# DRD1_MSN_C  DRD1_MSN_D       Inh_B       Inh_C       Inh_D       Inh_E 
+# "#2A4BF9"   "#FB3DD9"   "#7A0096"   "#854222"   "#A7F281"   "#0DFBFA" 
+# Excitatory       Inh_F 
+# "#5C6300"     "black" 
+
+
+spe$snRNA_predicted_CellType <- factor(x = spe$snRNA_predicted_CellType,
+                                       levels = c("DRD1_MSN_A","DRD1_MSN_B","DRD1_MSN_C","DRD1_MSN_D",
+                                                  "DRD2_MSN_A","DRD2_MSN_B",
+                                                  "Inh_A","Inh_B","Inh_C","Inh_D","Inh_E","Inh_F",
+                                                  "Excitatory",
+                                                  "Oligo","OPC",
+                                                  "Astrocyte_A","Astrocyte_B","Ependymal",
+                                                  "Microglia",
+                                                  "Endothelial"))
+
+
+library(ggplot2)
+
+max_score_box <- ggplot(colData(spe),aes(x = snRNA_predicted_CellType, 
+                                         y = predicted_max_score,
+                                         fill = snRNA_predicted_CellType)) +
+  geom_boxplot() +
+  geom_hline(yintercept = 0.50) +
+  theme_bw() +
+  scale_fill_manual(values = cluster_cols) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "none") +
+  labs(x = "Predicted Cell Type",
+       y = "Max Score")
+ggsave(max_score_box,filename = here("plots","06_label_transfer","snRNA_LabelTransfer",
+                                     "Max_score_boxplot.pdf"))
