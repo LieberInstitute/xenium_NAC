ca45f843707effa2e1acf528cbce386f9e53596b
Rphillips1995 <Rphillips11995@gmail.com>
2025-12-12 18:14:48 -0500

    Reciprocal PCA projection from visium spatial domains to Xenium for Br6436

---
+++ code/06_labeltransfer/09.2_visium_to_Br6436.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --job-name=09.2_Br6436
+#SBATCH --output=logs/09.2_Br6436.log
+#SBATCH --error=logs/09.2_Br6436.log 
+#SBATCH --mem=200G
+#SBATCH --mail-type=END
+#SBATCH --mail-user=Robert.Phillips@libd.org
+
+echo "********* Job Starts *********"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.5
+Rscript 09.2_visium_to_Br6436.R
+
+echo "********* Job Ends *********"
+date
