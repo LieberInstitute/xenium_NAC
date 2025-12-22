64c40146899eebefacd61eb5191130988f5570a6
Rphillips1995 <Rphillips11995@gmail.com>
2025-12-22 14:06:34 -0500

    Change name of cell id column

--- code/05_clustering/Banksy/Explore_non_spatial_clustering.R
+++ code/05_clustering/Banksy/Explore_non_spatial_clustering.R
@@ -16,7 +16,7 @@ files <- list.files(path = here("processed-data","05_Clustering","Banksy_Results
 #Read in the files
 files_list <- lapply(X = files,FUN = read.csv)
 names(files_list) <- basename(files)
-names_cols <-  c("row","cluster","cell_id")
+names_cols <-  c("row","cluster","id_cell")
 files_list <- lapply(files_list, setNames, names_cols)
 
 #How many clusters in each set
@@ -39,6 +39,7 @@ p <- lapply(files_list,FUN = function(x){
 ggsave(plot = p,
        file = here("plots","05_clustering","Banksy","Non_spatial_Number_clusters.pdf"))
 
+
 #make a dataframe of clusters
 #First make the names of the lists the second column
 for(i in names(files_list)){
@@ -47,13 +48,13 @@ for(i in names(files_list)){
 }
 
 #Are they all in the same order
-all(sapply(list(files_list[[2]]$cell_id, files_list[[3]]$cell_id,
-                files_list[[4]]$cell_id, files_list[[5]]$cell_id,
-                files_list[[6]]$cell_id), FUN = identical, files_list[[1]]$cell_id))
+all(sapply(list(files_list[[2]]$id_cell, files_list[[3]]$id_cell,
+                files_list[[4]]$id_cell, files_list[[5]]$id_cell,
+                files_list[[6]]$id_cell), FUN = identical, files_list[[1]]$id_cell))
 #[1] TRUE
 
 #Now create a dataframe that is the cell_id and all of the clustering options? 
-clustering_options <- data.frame(cell_id = files_list[[1]]$cell_id,
+clustering_options <- data.frame(id_cell = files_list[[1]]$id_cell,
                                  Lambda0_res0.25_louvain  = files_list[[1]]$Lambda0_res0.25_louvain,
                                  Lambda0_res0.5_louvain   = files_list[[2]]$Lambda0_res0.5_louvain,
                                  Lambda0_res0.75_louvain  = files_list[[3]]$Lambda0_res0.75_louvain,
@@ -63,3 +64,40 @@ clustering_options <- data.frame(cell_id = files_list[[1]]$cell_id,
 
 saveRDS(object = clustering_options,
         file = here("processed-data","05_Clustering","Non_spatial_results_combined.RDS"))
+
+
+#Load spe object
+spe <- readRDS(here("processed-data","02_build_spe","SPEs","spe_NormCounts.Rds"))
+
+#Subset for Br6660
+spe <- spe[,spe$Donor == "Br6660"]
+
+spe
+# class: SpatialExperiment 
+# dim: 366 2425841 
+# metadata(22): Samples Samples ... Samples Samples
+# assays(3): counts nucleus_normcounts cell_normcounts
+# rownames(366): ABCC9 ADAMTS12 ... ZBBX ZDHHC23
+# rowData names(8): ID Symbol ... subsets_any_neg subsets_GEX
+# colnames(2425841): Br6660_NAc1_580_1 Br6660_NAc1_580_2 ...
+# Br6660_Nac11_5580_201084 Br6660_Nac11_5580_201085
+# colData names(59): Sample Barcode ... cell_area.sf nucleus_area.sf
+# reducedDimNames(0):
+#   mainExpName: NULL
+# altExpNames(0):
+#   spatialCoords names(2) : x_final y_final
+# imgData names(1): sample_id
+
+identical(rownames(colData(spe)),clustering_options$id_cell)
+#[1] TRUE
+
+identical(colnames(spe),clustering_options$id_cell)
+#[1] TRUE
+
+colData(spe) <- cbind(colData(spe),clustering_options)
+
+identical(rownames(colData(spe)),colData(spe)$id_cell)
+#[1] TRUE
+
+identical(colnames(spe),colData(spe)$id_cell)
+#[1] TRUE
