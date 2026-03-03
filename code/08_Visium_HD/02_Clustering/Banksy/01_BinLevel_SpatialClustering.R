4c21709fd72c0488b360316f372558830d8f6540
Rphillips1995 <Rphillips11995@gmail.com>
2026-03-02 21:56:40 -0500

    Banksy clustering at 8um bins with lambda = 0.8 to perform domain findings

---
+++ code/08_Visium_HD/02_Clustering/Banksy/01_BinLevel_SpatialClustering.R
@@ -0,0 +1,82 @@
+#Goal: Run Banksy spatial clustering with parameters equating to spatial clustering for domain finding
+#cd /dcs05/lieber/marmaypag/xenium_NAC_LIBD4125/xenium_NAC/
+# module load conda_R/4.5
+#code modified from: 
+#https://github.com/LieberInstitute/spatialDLPFC_SCZ_XENIUM/blob/devel/code/analysis/03_clustering/01_spatial_domains_banksy.R and
+#https://www.bioconductor.org/packages/release/bioc/vignettes/Banksy/inst/doc/multi-sample.html
+library(SpatialExperiment)
+library(sessioninfo)
+library(ggplot2)
+library(escheR)
+library(Banksy)
+library(here)
+library(scran)
+
+
+#Read in the filtered spe object
+spe_filtered_path <- here(
+    "processed-data", "HD_Full_Analysis", "SPEs", "spe_norm_binQC_filtered.Rds"
+)
+
+spe <- readRDS(spe_filtered_path)
+
+#Remove any cells with 0 counts for all genes 
+#Remove cells with 0 counts
+spe <- spe[, colSums(counts(spe)) > 0]
+
+#Sanity check 
+table(colSums(counts(spe)) > 0)
+
+spe
+
+#Find top 4000 HVGs and subset
+top_hvgs <- getTopHVGs(modelGeneVar(spe), n = 2000)
+spe <- spe[top_hvgs, ]
+
+#######Banksy parameters
+lambda <- 0.8 #0.8 for spatial
+compute_agf <- TRUE 
+use_agf <- TRUE
+k_geom <- 50  #Increase k value to find larger domains. 
+
+#split spe by sample
+samples <- unique(spe$sample_id)
+spe_list <- list()
+for(i in samples){
+  spe_list[[i]] <- spe[,spe$sample_id == i]
+}
+
+
+#Banksy Step 1 
+message(paste0("Running computeBanksy - ", Sys.time()))
+spe_list <- lapply(spe_list,computeBanksy,
+                   assay_name = "logcounts",compute_agf = compute_agf,k_geom = k_geom)
+message(paste0("Finished computeBanksy - ", Sys.time()))
+
+
+#Merge SPEs
+spe_joint <- do.call(cbind, spe_list)
+rm(spe_list)
+gc()
+
+#Now run PCA and UMAP on the Banksy matrix
+message(paste0("Running PCA - ", Sys.time()))
+spe_joint <- runBanksyPCA(spe_joint,use_agf = use_agf, lambda = lambda, group = "sample_id",seed = 1000) #Vignette uses seed of 1000
+message(paste0("Finished PCA - ", Sys.time()))
+
+message(paste0("Running UMAP - ", Sys.time()))
+spe_joint <- runBanksyUMAP(spe_joint, use_agf = use_agf, lambda = lambda, seed = 1000)
+message(paste0("Finished UMAP - ", Sys.time()))
+
+#Save the object
+saveRDS(spe_joint, here("processed-data", "HD_Full_Analysis", "SPEs", "spe_bin_level_banksy_lambda_pt8.RDS"))
+
+###Reproduciblity
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+sessioninfo::session_info()
+
+
+
