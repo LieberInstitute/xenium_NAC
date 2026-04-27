eebf104d85335ffadfe9f6e7511000e91f7d8387
JianingYao <jianingyao619@gmail.com>
2026-04-26 22:56:43 -0400

    update with color code

--- code/05_clustering/Spateo/get_cell_type.r
+++ code/05_clustering/Spateo/get_cell_type.r
@@ -4,11 +4,27 @@
 library(SpatialExperiment)
 library(here)
 
-celltype_cols <- readRDS("/dcs05/lieber/marmaypag/xenium_NAC_LIBD4125/xenium_NAC/processed-data/05_Clustering/CellType_cols.Rds")
+CellType_cols <- readRDS(here("processed-data","05_Clustering","CellType_cols.Rds"))
 
-# old annotation
-# spe_annot <- readRDS("/dcs05/lieber/marmaypag/xenium_NAC_LIBD4125/xenium_NAC/processed-data/06_label_transfer/Objects/Banksy_CellTypes_transfer.Rds")
-# write.csv(spe_annot, file = "/dcs05/lieber/marmaypag/xenium_NAC_LIBD4125/xenium_NAC/processed-data/05_Clustering/Banksy_cell_types.csv", row.names = FALSE)
+#Rename colors 
+names(CellType_cols)[18] <- "Astrocyte_Oligo"
+names(CellType_cols)[22] <- "Microglia_Oligo"
+
+#Remove WM_A-C colors
+CellType_cols <- CellType_cols[-c(3:5)]
+
+#Add a color for WM
+CellType_cols <- c(CellType_cols,"orange")
+names(CellType_cols)[20] <- "WM"
+
+#     Excitatory     Microglia_A     D1_Island_B         Astro_A    Fibroblast_A 
+#       "#DB1C5F"       "#0D87E4"       "#00FF0D"       "#FD00FD"       "#FFA7E2" 
+#    Fibroblast_B        DRD2_MSN        DRD1_MSN         Astro_B       MSN_Oligo 
+#       "#2AFECA"       "#7AAA16"       "#9400FF"       "#823526"       "#F5DEC0" 
+#       Inh_PVALB             OPC     Microglia_B         Inh_SST Astrocyte_Oligo 
+#       "#93E5FF"       "#7A1699"       "#FF0DBC"       "#C4B3FB"       "#F80D2A" 
+#     D1_Island_A            CHAT       Ependymal Microglia_Oligo              WM 
+#       "#224B82"       "#FBA475"       "#73690D"       "#B62A7A"        "orange" 
 
 spe_annot <- readRDS("/dcs05/lieber/marmaypag/xenium_NAC_LIBD4125/xenium_NAC/processed-data/02_build_spe/SPEs/spe_celltype_v2.Rds")
 cell_types <- spe_annot$CellTypes
