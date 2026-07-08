0abc74488f2a069b4e6bd9dbf65b1a3157ad11eb
Rphillips1995 <Rphillips11995@gmail.com>
2026-07-08 14:03:48 -0400

    Perform MetaNeighbor analysis without Xenium

---
+++ code/MetaNeighbor/05_All_Species_MSNs_noXenium.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH --job-name=05_MSNs
+#SBATCH --output=logs/05_MSNs.log
+#SBATCH --error=logs/05_MSNs.log 
+#SBATCH --mem=100G
+#SBATCH --cpus-per-task=1
+#SBATCH --time=7-00:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=robert.phillips@libd.org
+
+echo "********* Job Starts *********"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.5
+Rscript 05_All_Species_MSNs_noXenium.R
+
+echo "********* Job Ends *********"
+date
+
