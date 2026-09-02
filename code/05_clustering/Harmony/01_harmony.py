4ed6017ebba571a020ca0e943f917c111aa9cc53
JianingYao <jianingyao619@gmail.com>
2026-09-02 12:24:40 -0400

    update harmony script (not used in the analysis)

--- code/05_clustering/Harmony/01_harmony.py
+++ code/05_clustering/Harmony/01_harmony.py
@@ -21,6 +21,10 @@ adata_all = ad.read_h5ad(git_root / "processed-data" / "02_build_spe" / "h5ad" /
 gene_mask = adata_all.var["Type"] == "Gene Expression"
 adata_all = adata_all[:, gene_mask].copy()
 
+DONOR = "Br6660"
+if DONOR != "All":
+    adata_all = adata_all[adata_all.obs["Donor"] == DONOR].copy()
+
 # Calculate PCA
 # adata_all.layers["counts"] = adata_all.X.copy()
 sc.pp.pca(adata_all, n_comps=30)
@@ -33,7 +37,8 @@ ho = hm.run_harmony(data_mat, meta_data, ['Sample'])
 # mapping back the result to the adata object
 adata_all.obsm['X_pca_harmony'] = ho.Z_corr.T
 
-adata_all.write(git_root / "processed-data" / "05_Clustering" / "Harmony.h5ad")
+# Save the integrated data for
+adata_all.write(git_root / "processed-data" / "05_Clustering" / f"Harmony_{DONOR}.h5ad")
 
 sc.pp.neighbors(adata_all, use_rep='X_pca_harmony')
 sc.tl.umap(adata_all)
